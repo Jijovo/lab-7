2b9cd52908f5991552d64e053fda43a86844a109
Jijovo <jijovo77@gmail.com>
2026-02-15 16:14:50 -0800

    display func + final

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@ string* reverseArray(string* arr, int size);
 void displayArray(string* arr, int size);
 
 int main() {
-
+    const int size = 5;
+    string* arr = new string[size];
+    *(arr + 0) = "Lotfy";
+    *(arr + 1) = "Mohamed";
+    *(arr + 2) = "Mustafa";
+    *(arr + 3) = "Ahmad";
+    *(arr + 4) = "Khaled";
+    cout << "Original Array: " << endl;
+    displayArray(arr, size);
+    cout << endl << "Reversed Array: " << endl;
+    displayArray(reverseArray(arr, size), size);
     return 0;
 }
 
@@ -16,4 +26,10 @@ string* reverseArray(string* arr, int size) {
         *(reversedArr + i) = *(arr + (size - 1 - i));
     }
     return reversedArr;
+}
+//Display function, recieves array and its size, then displays its elements
+void displayArray(string* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << endl;
+    }
 }
