8adb179de990182b9e09a12aaf2b3d08585e64e8
Jijovo <jijovo77@gmail.com>
2026-02-15 16:04:39 -0800

    initial

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//Prototypes
+string* reverseArray(string* arr, int size);
+void displayArray(string* arr, int size);
+
+int main() {
+
+    return 0;
+}
+
