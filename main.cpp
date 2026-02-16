776a64fc43e70e3a515762153a6d0bf81b478a1f
Jijovo <jijovo77@gmail.com>
2026-02-15 16:07:55 -0800

    reverse func

--- main.cpp
+++ main.cpp
@@ -9,3 +9,11 @@ int main() {
     return 0;
 }
 
+//Reverse function, recieves array, creates reversed array and returns its pointer
+string* reverseArray(string* arr, int size) {
+    string* reversedArr = new string[size];
+    for (int i = 0; i < size; i++) {
+        *(reversedArr + i) = *(arr + (size - 1 - i));
+    }
+    return reversedArr;
+}
